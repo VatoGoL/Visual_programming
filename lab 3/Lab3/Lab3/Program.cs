0d6d8ebfd20a471935e5cf6e3ff9a8f912719c1a
Иван Шульженко <ilitetop@yandex.ru>
2022-03-02 19:28:23 +0700

    Лабораторная 3

---
+++ lab 3/Lab3/Lab3/Program.cs
@@ -0,0 +1,75 @@
+﻿using Lab3;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        RomanNumber r = new RomanNumber(65535);
+        Console.WriteLine(r.ToString());
+        int n = 5;
+        Random random = new Random();
+        RomanNumber[] A_Test = {
+            new RomanNumber((ushort)(random.Next(0, ushort.MaxValue)/3)),
+            new RomanNumber((ushort)(random.Next(0, ushort.MaxValue)/3)),
+            new RomanNumber((ushort)(random.Next(0, ushort.MaxValue)/3)),
+            new RomanNumber((ushort)(random.Next(0, ushort.MaxValue)/3)),
+            new RomanNumber((ushort)(random.Next(0, ushort.MaxValue)/3))
+        };
+
+        try
+        {
+            RomanNumber result = A_Test[0] / A_Test[1]; // /
+            Console.WriteLine("Result : " + result.ToString());
+        }
+        catch (RomanNumberException e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            try
+            {
+                RomanNumber result = A_Test[2] + A_Test[3]; //+
+                Console.WriteLine("Result : " + result.ToString());
+            }
+            catch (RomanNumberException e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                try
+                {
+                    RomanNumber result = A_Test[3] - A_Test[4]; //-
+                    Console.WriteLine("Result : " + result.ToString());
+                }
+                catch (RomanNumberException e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    try
+                    {
+                        RomanNumber result = A_Test[0] * A_Test[1]; // *
+                        Console.WriteLine("Result : " + result.ToString());
+                    }
+                    catch (RomanNumberException e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                    finally
+                    {
+                        Array.Sort(A_Test);
+                        Console.WriteLine("\nОтсортированный массив:");
+                        for (int i = 0; i < A_Test.Length; i++)
+                        {
+                            Console.WriteLine(A_Test[i].ToString());
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ lab 3/Lab3/Lab3Tests3/RomanNumberTests.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+
+        [TestMethod()]
+        public void RomanNumberTest()
+        {
+            RomanNumber value = new RomanNumber(10);
+            string result = "X";
+
+            Assert.AreEqual(result, value.ToString());
+        }
+        
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            RomanNumber one = new RomanNumber(10);
+            RomanNumber two = new RomanNumber(20);
+            RomanNumber result = new RomanNumber(30);
+
+            RomanNumber factResult = one + two;
+            
+            Assert.AreEqual(factResult.ToString(),result.ToString());
+        }
+
+        [TestMethod()]
+        public void AddMaxTest()
+        {
+            RomanNumber one = new RomanNumber(65500);
+            RomanNumber two = new RomanNumber(40);
+
+            Assert.ThrowsException<RomanNumberException>(() => one+two);
+        }
+
+        [TestMethod()]
+        public void SubTest()
+        {
+            RomanNumber one = new RomanNumber(20);
+            RomanNumber two = new RomanNumber(10);
+            RomanNumber result = new RomanNumber(10);
+
+            RomanNumber factResult = one - two;
+
+            Assert.AreEqual(factResult.ToString(), result.ToString());
+        }
+
+        [TestMethod()]
+        public void SubMinTest()
+        {
+            RomanNumber one = new RomanNumber(10);
+            RomanNumber two = new RomanNumber(40);
+
+            Assert.ThrowsException<RomanNumberException>(() => one - two);
+        }
+
+        [TestMethod()]
+        public void MulTest()
+        {
+            RomanNumber one = new RomanNumber(20);
+            RomanNumber two = new RomanNumber(10);
+            RomanNumber result = new RomanNumber(200);
+
+            RomanNumber factResult = one * two;
+
+            Assert.AreEqual(factResult.ToString(), result.ToString());
+        }
+
+        [TestMethod()]
+        public void MulMaxTest()
+        {
+            RomanNumber one = new RomanNumber(65000);
+            RomanNumber two = new RomanNumber(2);
+
+            Assert.ThrowsException<RomanNumberException>(() => one * two);
+        }
+
+        [TestMethod()]
+        public void DivTest()
+        {
+            RomanNumber one = new RomanNumber(20);
+            RomanNumber two = new RomanNumber(10);
+            RomanNumber result = new RomanNumber(2);
+
+            RomanNumber factResult = one / two;
+
+            Assert.AreEqual(factResult.ToString(), result.ToString());
+        }
+
+        [TestMethod()]
+        public void DivNULLTest()
+        {
+            RomanNumber one = new RomanNumber(35);
+            RomanNumber two = new RomanNumber(0);
+
+            Assert.ThrowsException<RomanNumberException>(() => one / two);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            RomanNumber value = new RomanNumber(652);
+            string result = "DCLII";
+
+            Assert.AreEqual(result, value.ToString());
+        }
+
+        [TestMethod()]
+        public void ToStringNULLTest()
+        {
+            RomanNumber value = new RomanNumber(0);
+            string result = "";
+
+            Assert.AreEqual(result, value.ToString());
+        }
+
+        [TestMethod()]
+        public void ToStringMaxTest()
+        {
+            RomanNumber value = new RomanNumber(65535);
+            string result = "_L_X_VDXXXV";
+
+            Assert.AreEqual(result, value.ToString());
+        }
+
+        [TestMethod()]
+        public void CloneEqualTest()
+        {
+            RomanNumber one = new RomanNumber(5);
+            RomanNumber two = new RomanNumber(666);
+            one = two;
+
+            Assert.AreEqual(one, two);
+        }
+        
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            RomanNumber[] A = {
+                new RomanNumber(5),
+                new RomanNumber(6),
+                new RomanNumber(2),
+                new RomanNumber(184),
+                new RomanNumber(1)
+            };
+
+            RomanNumber[] Result = {
+                new RomanNumber(1),
+                new RomanNumber(2),
+                new RomanNumber(5),
+                new RomanNumber(6),
+                new RomanNumber(184)
+            };
+            Array.Sort(A);
+            for(int i = 0; i < A.Length; i++)
+            {
+                Assert.AreEqual(A[i].ToString(), Result[i].ToString());
+            }
+        }
+    }
+}
